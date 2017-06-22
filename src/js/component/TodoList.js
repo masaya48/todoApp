bf0c67271cfcc530eed759e1b407e2588cde4aea
masaya48 <msya0408@gmail.com>
2017-06-23 00:53:49 +0900

    react-router導入

---
+++ src/js/component/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class TodoList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Text>todoList</Text>
+      </View>
+    );
+  }
+}
