0f976c91a0c15ddb235d3484f9f2fce441bb361a
masaya48 <msya0408@gmail.com>
2017-07-25 01:01:46 +0900

    action追加

--- src/js/actions/todo.js
+++ src/js/actions/todo.js
@@ -1,8 +1,19 @@
 export const TEXT_UPDATE = 'TEXT_UPDATE';
+export const ADD_TODO = 'ADD_TODO';
 
 export const fetchTextUpdate = text => {
   return{
     type: TEXT_UPDATE,
+    text: text,
+  }
+}
+
+let nextId = 0;
+
+export const addTodo = text => {
+  return{
+    type: ADD_TODO,
+    id: nextId++,
     text: text,
   }
 }
--- src/js/reducers/todo-reducer.js
+++ src/js/reducers/todo-reducer.js
@@ -11,7 +11,6 @@ const initialState = {
 export default function todoReducer(state = initialState, action = {}){
   switch (action.type){
     case TEXT_UPDATE:
-      // 空のObnectにマージする
       return Object.assign({}, state, {
         text: action.text,
       })
